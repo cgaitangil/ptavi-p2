80b6018e0f5622222c0a9cf65267e1d598d284c1
Carlos Gaitan Gil <cgaitan@gamma04.aulas.gsyc.urjc.es>
2016-09-26 19:13:15 +0200

    Revisión pep8 hecha

--- calcplusplus.py
+++ calcplusplus.py
@@ -5,31 +5,36 @@ import sys
 import calcoohija
 import csv
 
+
 if __name__ == '__main__':
 
-    lineas = csv.reader(open(sys.argv[1]))    #Lo guarda como lista con los 
-                                                #componentes ya separados por
-                                                #linea y sin \n
+    with open('operaciones', 'r') as fichero:
+        operaciones = csv.reader(fichero)
 
-    calc = calcoohija.CalculadoraHija() #sin () seria puntero
-    
-    DiccOper = {'suma': calc.plus, 'resta': calc.minus, 'multiplica': calc.mul, 'divide': calc.div}               
+    calc = calcoohija.CalculadoraHija()
 
-    for linea in lineas:
-    
-        operandos = linea[1:]         #Le quitamos el primer componente
-        operacion = linea[0]          #La operacion sera el perimer comp.
-        
+    DiccOper = {'suma': calc.plus, 'resta': calc.minus,
+                'multiplica': calc.mul, 'divide': calc.div}
+
+    for linea in operaciones:
+
+        elems_lista = linea.split(',')
+
+        operandos = elems_lista[1:]    # Le quitamos el primer componente
+        operandos[-1] = operandos[-1][:-1]    # Quitar el \n al último comp.
+        operacion = elems_lista[0]    # La operacion sera el perimer comp.
+
         resultado = int(operandos[0])
-        operandos = operandos[1:]   #Empieza a sumar desde el 2º comp.
-        
-        for operando in operandos:   
-            if operacion == 'divide' and operando == 0:
+        operandos = operandos[1:]    # Empieza a sumar desde el 2º comp.
+
+        for operando in operandos:
+            if operacion == 'divide' and int(operando) == 0:
                 sys.exit('Division by 0 is not allowed')
             else:
                 try:
                     resultado = DiccOper[operacion](resultado, int(operando))
                 except KeyError:
-                    sys.exit('Operación sólo puede ser suma, resta, multiplica o divide')           
-       
+                    sys.exit('Operación sólo puede ser suma, resta, \
+multiplica o divide')
+
         print('Resultado = ' + str((resultado)))
