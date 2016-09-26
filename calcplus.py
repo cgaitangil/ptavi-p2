e66cbdd45a53f38e4aa0f51333aa738b383b926a
Carlos Gaitan Gil <cgaitan@alpha08.aulas.gsyc.urjc.es>
2016-09-26 11:45:13 +0200

    Solucionado punteros

--- calcplus.py
+++ calcplus.py
@@ -6,11 +6,11 @@ import calcoohija
 
 if __name__ == '__main__':
 
-    fichero = open('/home/cgaitan/ptavi-p2/operaciones.txt', 'r')
+    fichero = open(sys.argv[1], 'r')
 
-    lineas = fichero.readlines()
+    lineas = fichero.readlines() 
 
-    calc = calcoohija.CalculadoraHija
+    calc = calcoohija.CalculadoraHija() #sin () seria puntero
     
     DiccOper = {'suma': calc.plus, 'resta': calc.minus, 'multiplica': calc.mul, 'divide': calc.div}               
 
@@ -21,8 +21,6 @@ if __name__ == '__main__':
         operandos = elems_lista[1:]         #Le quitamos el primer componente
         operandos[-1] = operandos[-1][:-1]  #Le quitamos el \n al último comp.
         operacion = elems_lista[0]          #La operacion sera el perimer comp.
-        
-        print(operandos)
         
         resultado = int(operandos[0])
         operandos = operandos[1:]   #Empieza a sumar desde el 2º comp.
@@ -36,4 +34,4 @@ if __name__ == '__main__':
                 except KeyError:
                     sys.exit('Operación sólo puede ser suma, resta, multiplica o divide')           
        
-        print('Resultado = ' + str(int(resultado)))
+        print('Resultado = ' + str((resultado)))
