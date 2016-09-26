d48e90cdc5111f090c30f147ff6a11060f3e64b1
Carlos Gaitan Gil <cgaitan@gamma04.aulas.gsyc.urjc.es>
2016-09-26 19:13:07 +0200

    Revisión pep8 hecha

--- calcoohija.py
+++ calcoohija.py
@@ -4,7 +4,8 @@
 import sys
 import calcoo
 
-class CalculadoraHija(calcoo.Calculadora) :
+
+class CalculadoraHija(calcoo.Calculadora):
 
     def div(self, op1, op2):
         if op2 == 0:
@@ -16,24 +17,25 @@ class CalculadoraHija(calcoo.Calculadora) :
         return op1 * op2
 
 if __name__ == '__main__':
-    
-    calcoohija = CalculadoraHija() #Contiene los self de manera estricta
-                                   #ya que es una variable
-    try:                           #sin () seria puntero
+
+    calcoohija = CalculadoraHija()
+# Contiene self en sus métodos ya que es una variable. Sin () sería puntero.
+
+    try:
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit('Non numerical parameters')
-     
+
     if sys.argv[2] == 'divide':
         resultado = calcoohija.div(operando1, operando2)
     elif sys.argv[2] == 'multiplica':
         resultado = calcoohija.mul(operando1, operando2)
     elif sys.argv[2] == 'suma':
-        resultado =  calcoohija.plus(operando1, operando2)
+        resultado = calcoohija.plus(operando1, operando2)
     elif sys.argv[2] == 'resta':
         resultado = calcoohija.minus(operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser suma, resta, multiplica o divide')
-        
+
     print('Resultado = ' + str(resultado))
