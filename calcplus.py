8ccacd935f897def91d79d88aca952201b4f479e
Carlos Gaitan Gil <cgaitan@gamma04.aulas.gsyc.urjc.es>
2016-09-26 19:13:12 +0200

    Revisión pep8 hecha

--- calcplus.py
+++ calcplus.py
@@ -8,30 +8,32 @@ if __name__ == '__main__':
 
     fichero = open(sys.argv[1], 'r')
 
-    lineas = fichero.readlines() 
+    lineas = fichero.readlines()
 
-    calc = calcoohija.CalculadoraHija() #sin () seria puntero
-    
-    DiccOper = {'suma': calc.plus, 'resta': calc.minus, 'multiplica': calc.mul, 'divide': calc.div}               
+    calc = calcoohija.CalculadoraHija()    # sin () seria puntero
+
+    DiccOper = {'suma': calc.plus, 'resta': calc.minus,
+                'multiplica': calc.mul, 'divide': calc.div}
 
     for linea in lineas:
-    
+
         elems_lista = linea.split(',')
-        
-        operandos = elems_lista[1:]         #Le quitamos el primer componente
-        operandos[-1] = operandos[-1][:-1]  #Le quitamos el \n al último comp.
-        operacion = elems_lista[0]          #La operacion sera el perimer comp.
-        
+
+        operandos = elems_lista[1:]        # Le quitamos el primer componente
+        operandos[-1] = operandos[-1][:-1]  # Le quitamos el \n al último comp.
+        operacion = elems_lista[0]        # La operacion sera el perimer comp.
+
         resultado = int(operandos[0])
-        operandos = operandos[1:]   #Empieza a sumar desde el 2º comp.
-        
-        for operando in operandos:   
+        operandos = operandos[1:]   # Empieza a sumar desde el 2º comp.
+
+        for operando in operandos:
             if operacion == 'divide' and int(operando) == 0:
                 sys.exit('Division by 0 is not allowed')
             else:
                 try:
                     resultado = DiccOper[operacion](resultado, int(operando))
                 except KeyError:
-                    sys.exit('Operación sólo puede ser suma, resta, multiplica o divide')           
-       
+                    sys.exit('Operación sólo puede ser suma, resta, multiplica\
+ o divide')
+
         print('Resultado = ' + str((resultado)))
