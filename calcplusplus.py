ef142d1d186a424de0bdc10f07d39bf649f32109
cgaitangil <cgaitangil@gmail.com>
2016-09-26 14:19:50 +0200

    Hecho

--- calcplusplus.py
+++ calcplusplus.py
@@ -5,32 +5,26 @@ import sys
 import calcoohija
 import csv
 
-
-
 if __name__ == '__main__':
 
-    with open('operaciones', 'r') as fichero:
-         operaciones= csv.reader(fichero)
-         
-    
+    lineas = csv.reader(open(sys.argv[1]))    #Lo guarda como lista con los 
+                                                #componentes ya separados por
+                                                #linea y sin \n
 
-    calc = calcoohija.CalculadoraHija()
+    calc = calcoohija.CalculadoraHija() #sin () seria puntero
     
     DiccOper = {'suma': calc.plus, 'resta': calc.minus, 'multiplica': calc.mul, 'divide': calc.div}               
 
-    for linea in operaciones:
+    for linea in lineas:
     
-        elems_lista = linea.split(',')
-        
-        operandos = elems_lista[1:]         #Le quitamos el primer componente
-        operandos[-1] = operandos[-1][:-1]  #Le quitamos el \n al último comp.
-        operacion = elems_lista[0]          #La operacion sera el perimer comp.
+        operandos = linea[1:]         #Le quitamos el primer componente
+        operacion = linea[0]          #La operacion sera el perimer comp.
         
         resultado = int(operandos[0])
         operandos = operandos[1:]   #Empieza a sumar desde el 2º comp.
         
         for operando in operandos:   
-            if operacion == 'divide' and int(operando) == 0:
+            if operacion == 'divide' and operando == 0:
                 sys.exit('Division by 0 is not allowed')
             else:
                 try:
